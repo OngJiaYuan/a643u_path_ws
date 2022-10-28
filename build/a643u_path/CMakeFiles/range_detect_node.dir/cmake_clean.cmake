ee7e7b0b9261e3cb2a55fe00846b82e6f1dac7be
Ongjy <jiayuan.ong@kabam.ai>
2022-10-28 15:32:37 +0800

    "initial commit"

---
+++ build/a643u_path/CMakeFiles/range_detect_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/jy/a643u_path_ws/devel/lib/a643u_path/range_detect_node"
+  "/home/jy/a643u_path_ws/devel/lib/a643u_path/range_detect_node.pdb"
+  "CMakeFiles/range_detect_node.dir/src/range_detect_node.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/range_detect_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/a643u_path/catkin_generated/pkg.develspace.context.pc.py
@@ -0,0 +1,8 @@
+# generated from catkin/cmake/template/pkg.context.pc.in
+CATKIN_PACKAGE_PREFIX = ""
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/jy/a643u_path_ws/src/a643u_path/include".split(';') if "/home/jy/a643u_path_ws/src/a643u_path/include" != "" else []
+PROJECT_CATKIN_DEPENDS = "roscpp;sensor_msgs;nav_msgs;geometry_msgs;std_msgs".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
+PROJECT_NAME = "a643u_path"
+PROJECT_SPACE_DIR = "/home/jy/a643u_path_ws/devel"
+PROJECT_VERSION = "0.0.0"
