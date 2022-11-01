2e380e1e0957f3c9cd7599ce561c5fb0b00817cd
Ongjy <jiayuan.ong@kabam.ai>
2022-11-02 02:13:31 +0800

    changes to the publisher

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jy/a643u_path_ws/src
-# Build directory: /home/jy/a643u_path_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("a643u_path")
--- build/a643u_path/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jy/a643u_path_ws/src/a643u_path
-# Build directory: /home/jy/a643u_path_ws/build/a643u_path
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
